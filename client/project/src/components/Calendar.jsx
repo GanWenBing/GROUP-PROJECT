dc39223b561442e1f7c3dad3c8845f1840bbb91a
Jabs <98640151+JT142@users.noreply.github.com>
2022-10-25 17:11:58 +0800

    Added comments for calendar page

--- client/project/src/components/Calendar.jsx
+++ client/project/src/components/Calendar.jsx
@@ -5,6 +5,7 @@ const Calendar = () => {
         <>
             <NavBar />
             <h2> Insert Calendar here </h2>
+            <p> Not sure if this page is needed. If there is no planned use for it, can remove  :)</p>
         </>
     )
 }
