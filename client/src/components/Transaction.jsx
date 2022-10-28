e5b341e794ef5203ddfe915b7c6e6d3c73ff766c
Jabelle <jabelletsm@gmail.com>
2022-10-28 17:17:40 +0800

    Attempted to fix Delete Transaction bug

--- client/src/components/Transaction.jsx
+++ client/src/components/Transaction.jsx
@@ -1,22 +1,23 @@
-import { GlobalContext } from "../context/GlobalState"
-import { useContext } from "react";
+
+// import { GlobalContext } from "../context/GlobalState"
+// import { useContext } from "react";
 
 
-const Transaction = ({ transaction }) => {
-    const sign = transaction.amount < 0 ? '-' : '+';
-    const { deleteTransaction } = useContext(GlobalContext);
+// const Transaction = ({ transaction }) => {
+//     const sign = transaction.amount < 0 ? '-' : '+';
+//     const { deleteTransaction } = useContext(GlobalContext);
 
-    return (
-        <>
-            <li className={transaction.amount < 0 ? "minus" : "plus"}>
-                {transaction.text}
-                <span> {sign} ${Math.abs(transaction.amount)} </span>
-                <button 
-                    className="delete-btn"
-                    onClick= {() => deleteTransaction(transaction._id)}>x</button>
-            </li>
-        </>
-    )
-}
+//     return (
+//         <>
+//             <li className={transaction.amount < 0 ? "minus" : "plus"}>
+//                 {transaction.text}
+//                 <span> {sign} ${Math.abs(transaction.amount)} </span>
+//                 <button 
+//                     className="delete-btn"
+//                     onClick= {() => deleteTransaction(transaction._id)}>x</button>
+//             </li>
+//         </>
+//     )
+// }
 
-export default Transaction
+// export default Transaction
--- client/src/components/TransactionList.jsx
+++ client/src/components/TransactionList.jsx
@@ -1,128 +1,123 @@
+import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
 import { GlobalContext } from "../context/GlobalState"
-import { useContext, useEffect } from "react"
-import Transaction from "./Transaction"
+import { useContext } from "react";
 
-const TransactionList = () => {
-    const { transactions, getTransactions } = useContext(GlobalContext)
+const TransactionList = ({transaction}) => {
+  const [list, setList] = useState([])
+  const { deleteTransaction } = useContext(GlobalContext);
 
-    useEffect(() => {
-      getTransactions();
-    }, []);
+  useEffect(() => {
+    const userinfo = JSON.parse(localStorage.getItem("userInfo"))
+    const id = userinfo.id
+    fetch(`http://localhost:3000/expense/listexpense/${id}`)
+      .then((response) => response.json())
+      .then((data) =>
+        setList(data));
+  }, []);
 
-    return (
-        <>
-            <h3>History</h3>
-            <ul className="list">
-                {transactions.map(transaction => (<Transaction key={transaction.id} transaction = { transaction } />))}
-            </ul>
+  const handleClick = async () => {
+    console.log("Delete")
+    await fetch("http://localhost:3000/expense/listexpense/:id", {
+      method: "DELETE",
+      headers: {
+        'Content-Type': "application/json"
+      },
+      body: JSON.stringify(transaction)
+    })
+    const json = await response.json()
+      .then((response) => {
+        if (response.ok) {
+          deleteTransaction()
+          alert('expense deleted')
+        } else {
+          console.log("Oops something's wrong")
+        }
+        return response.json()
+      })
+      .then((data) => {
+        console.log(data)
+      });
 
-        </>
-    )
+  }
+
+  return (
+    <>
+      <h1>TrackIt</h1>
+      <div class="overflow-x-auto relative">
+        <table class="w-full text-sm text-left text-gray-800 dark:text-gray-400">
+          <thead class="text-xs text-gray-800 uppercase bg-white-50 dark:bg-white-700 dark:text-gray-800">
+            <tr>
+              <th scope="col" class="py-3 px-6">
+                Title
+              </th>
+              <th scope="col" class="py-3 px-6">
+                Category
+              </th>
+              <th scope="col" class="py-3 px-6">
+                Description
+              </th>
+              <th scope="col" class="py-3 px-6">
+                Date
+              </th>
+              <th scope="col" class="py-3 px-6">
+                Amount
+              </th>
+              <th scope="col" class="py-3 px-6">
+                Edit
+              </th>
+              <th scope="col" class="py-3 px-6">
+                Remove
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {list.map((item) => {
+              return (
+                <tr class="bg-white border-b dark:bg-white-800 dark:border-gray-100" key={item._id}>
+                  <td class="py-4 px-6">{item.title}</td>
+                  <td class="py-4 px-6">{item.category}</td>
+                  <td class="py-4 px-6">{item.description}</td>
+                  <td class="py-4 px-6">{item.date}</td>
+                  <td class="py-4 px-6">{item.amount}</td>
+                  <td class="py-4 px-6 text-gray-1500 hover:text-black"><button>edit</button></td>
+                  <td class="py-4 px-6 text-gray-1000 hover:text-black"><button onClick={() => deleteTransaction(transaction._id)}> remove </button> </td>
+
+              </tr>
+          )
+          })}
+
+        </tbody>
+
+      </table>
+    </div>
+  </>
+)
 }
 
 export default TransactionList
 
-// import React from 'react'
-// import { useState } from 'react'
-// import { useEffect } from 'react'
+
 // import { GlobalContext } from "../context/GlobalState"
-// import { useContext } from "react";
+// import { useContext } from "react"
+// import Transaction from "./Transaction"
 
 // const TransactionList = () => {
-//   const [list, setList] = useState([])
-//   const { deleteTransaction } = useContext(GlobalContext);
+//     const { transactions } = useContext(GlobalContext)
 
-//   useEffect(() => {
-//     const userinfo = JSON.parse(localStorage.getItem("userInfo"))
-//     const id = userinfo.id
-//     fetch(`http://localhost:3000/expense/listexpense/${id}`)
-//       .then((response) => response.json())
-//       .then((data) =>
-//         setList(data));
-//   }, []);
+//     return (
+//         <>
+//             <h3>History</h3>
+//             <ul className="list">
+//                 {transactions.map(transaction => (<Transaction key={transaction.id} transaction = { transaction } />))}
+//             </ul>
 
-//   const handleClick = async () => {
-//     console.log("Delete")
-//     await fetch("http://localhost:3000/expense/listexpense/:id", {
-//       method: "DELETE",
-//       headers: {
-//         'Content-Type': "application/json"
-//       },
-//       body: JSON.stringify(transaction)
-//     })
-//       const json = await response.json()
-//       .then((response) => {
-//         if (response.ok) {
-//           deleteTransaction()
-//           alert('expense deleted')
-//         } else {
-//           console.log("Oops something's wrong")
-//         }
-//         return response.json()
-//       })
-//       .then((data) => {
-//         console.log(data)
-//       });
-
-//   }
-
-// return (
-//   <>
-//     <h1>TrackIt</h1>
-//     <div class="overflow-x-auto relative">
-//       <table class="w-full text-sm text-left text-gray-800 dark:text-gray-400">
-//         <thead class="text-xs text-gray-800 uppercase bg-white-50 dark:bg-white-700 dark:text-gray-800">
-//           <tr>
-//             <th scope="col" class="py-3 px-6">
-//               Title
-//             </th>
-//             <th scope="col" class="py-3 px-6">
-//               Category
-//             </th>
-//             <th scope="col" class="py-3 px-6">
-//               Description
-//             </th>
-//             <th scope="col" class="py-3 px-6">
-//               Date
-//             </th>
-//             <th scope="col" class="py-3 px-6">
-//               Amount
-//             </th>
-//             <th scope="col" class="py-3 px-6">
-//               Edit
-//             </th>
-//             <th scope="col" class="py-3 px-6">
-//               Remove
-//             </th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {list.map((item) => {
-//             return (
-//               <tr class="bg-white border-b dark:bg-white-800 dark:border-gray-100" key={item._id}>
-//                 <td class="py-4 px-6">{item.title}</td>
-//                 <td class="py-4 px-6">{item.category}</td>
-//                 <td class="py-4 px-6">{item.description}</td>
-//                 <td class="py-4 px-6">{item.date}</td>
-//                 <td class="py-4 px-6">{item.amount}</td>
-//                 <td class="py-4 px-6 text-gray-1500 hover:text-black"><button>edit</button></td>
-//                 <td class="py-4 px-6 text-gray-1000 hover:text-black"><button onClick={handleClick}>remove</button></td>
-
-//               </tr>
-//             )
-//           })}
-
-//         </tbody>
-
-//       </table>
-//     </div>
-//   </>
-// )
+//         </>
+//     )
 // }
 
 // export default TransactionList
-
-
 
 
 
