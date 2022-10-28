ac44c4677d6893ca29fce30bcd23bd016899c376
Jabelle <jabelletsm@gmail.com>
2022-10-28 17:28:30 +0800

    Fixed IncomeExpenses and Balance components

--- client/src/components/Balance.jsx
+++ client/src/components/Balance.jsx
@@ -1,11 +1,20 @@
-import { GlobalContext } from "../context/GlobalState"
-import { useContext } from "react";
+import { useEffect, useState } from "react";
 
 const Balance = () => {
-    const { transactions } = useContext(GlobalContext);
+
+    const [list, setList] = useState([])
+
+    useEffect(() => {
+        const userinfo = JSON.parse(localStorage.getItem("userInfo"))
+        const id = userinfo.id
+        fetch(`http://localhost:3000/expense/listexpense/${id}`)
+          .then((response) => response.json())
+          .then((data) =>
+            setList(data));
+      }, []);
 
     // map over all the amounts and use reduce to add them together
-    const amounts = transactions.map(transaction => transaction.amount)
+    const amounts = list.map(transaction => transaction.amount)
     const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
     return (
@@ -17,4 +26,26 @@ const Balance = () => {
 }
 
 export default Balance
+
+
+
+// import { GlobalContext } from "../context/GlobalState"
+// import { useContext } from "react";
+
+// const Balance = () => {
+//     const { transactions } = useContext(GlobalContext);
+
+//     // map over all the amounts and use reduce to add them together
+//     const amounts = transactions.map(transaction => transaction.amount)
+//     const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+//     return (
+//         <>
+//             <h4> Your Balance </h4>
+//             <h1> ${total} </h1>
+//         </>
+//     )
+// }
+
+// export default Balance
 
--- client/src/components/IncomeExpenses.jsx
+++ client/src/components/IncomeExpenses.jsx
@@ -1,10 +1,18 @@
-import { GlobalContext } from "../context/GlobalState"
-import { useContext } from "react"
+import { useEffect, useState } from "react";
 
 const IncomeExpenses = () => {
-    const { transactions } = useContext(GlobalContext)
+    const [list, setList] = useState([])
 
-    const amounts = transactions.map(transaction => transaction.amount);
+    useEffect(() => {
+        const userinfo = JSON.parse(localStorage.getItem("userInfo"))
+        const id = userinfo.id
+        fetch(`http://localhost:3000/expense/listexpense/${id}`)
+          .then((response) => response.json())
+          .then((data) =>
+            setList(data));
+      }, []);
+
+    const amounts = list.map(transaction => transaction.amount);
 
     const income = amounts
         .filter(item => item > 0)
@@ -34,3 +42,42 @@ const IncomeExpenses = () => {
 }
 
 export default IncomeExpenses
+
+
+
+// import { GlobalContext } from "../context/GlobalState"
+// import { useContext } from "react"
+
+// const IncomeExpenses = () => {
+//     const { transactions } = useContext(GlobalContext)
+
+//     const amounts = transactions.map(transaction => transaction.amount);
+
+//     const income = amounts
+//         .filter(item => item > 0)
+//         .reduce((acc, item) => (acc += item), 0)
+//         .toFixed(2);
+
+//     const expense = (amounts
+//         .filter(item => item < 0)
+//         .reduce((acc,item) => (acc += item),0) * -1) 
+//         .toFixed(2)
+
+//     return (
+//         <>
+//             <div className="inc-exp-container">
+//                 <div>
+//                     <h4>Income</h4>
+//                     <p className="money plus"> ${income} </p>
+//                 </div>
+//                 <div>
+//                     <h4>Expense</h4>
+//                     <p className="money minus"> ${expense}</p>
+//                 </div>
+//             </div>
+//         </>
+//     )
+
+// }
+
+// export default IncomeExpenses
