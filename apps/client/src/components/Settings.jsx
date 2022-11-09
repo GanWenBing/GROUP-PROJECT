fbcd369c9f366fcf2750f6e6a3fb0bf66641bc92
gan <wenbing_1992@hotmail.com>
2022-11-09 22:14:41 +0800

    update

--- apps/client/src/components/Settings.jsx
+++ apps/client/src/components/Settings.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 const Settings = () => {
     const navigate = useNavigate()
    // const [shouldFetch, setShouldFetch] = useState(true)
-    const [update, setUpdate] = useState(true)
+    //const [update, setUpdate] = useState(true)
     const [profile, setProfile] = useState({})
     //const [shouldFetch, setShouldFetch] = useState(true)
     const [error, setError] = useState()
@@ -30,8 +30,8 @@ const Settings = () => {
         const userinfo = JSON.parse(localStorage.getItem("userInfo"))
         const id = userinfo.id
         console.log(id)
-        const { Username, Password, ConfirmPassword, Email } = profile
-        console.log(Password)
+        const { Username, Password, ConfirmPassword, Email } = profile;
+        console.log(Password);
         if (Password != ConfirmPassword) {
             console.log("Hi")
             return setError("Password did not match")    
@@ -57,7 +57,7 @@ const Settings = () => {
                         navigate('/')
                     } else {
                         console.log("Invalid, pls try again")
-                        setUpdate(false)
+                        // setUpdate(false)
                     }
                     return response.json()
                 })
@@ -85,7 +85,53 @@ const Settings = () => {
     return (
         <>
             <NavBar />
-            {
+            <div className="min-h-screen py-10">
+                    <div className="container mx-auto">
+                        <div className=" lg:flex-row w-full lg:w-8/12 bg-white rounded-xl mx-auto shadow-lg overflow-hidden">
+                            <div className="">
+                                <h2 className="text-3xl mb-4">Edit User</h2>
+                                <form onSubmit={handleSave}>
+
+                                    <div className="mt-5">
+                                        <label>
+                                            Username:
+                                        </label>
+                                        <input name="Username" placeholder={profile.Username} value={profile.Username} onChange={setdata} className="border border-gray-400 py-1 px-2 w-full"></input>
+
+                                    </div>
+
+                                    <div className="mt-5">
+                                        <label>
+                                            New Password: </label>
+                                        <input name="Password" type="password" onChange={setdata} className="border border-gray-400 py-1 px-2 w-full"></input>
+                                    </div>
+                                    <div className="mt-5">
+                                        <label>
+                                            Confirm New Password: </label>
+                                        <input name="ConfirmPassword" type="password" onChange={setdata} className="border border-gray-400 py-1 px-2 w-full"></input>
+                                    </div>
+
+                                    <div className="mt-5">
+                                        <label>
+                                            Email Address:
+                                        </label>
+                                        <input name="Email" placeholder={profile.Email} value={profile.Email} onChange={setdata} className="border border-gray-400 py-1 px-2 w-full"></input>
+                                    </div>
+
+
+                                    <div className="mt-5">
+                                        <button className="w-full bg-purple-500 py-3 text-center text-white">Update</button>
+                                    </div>
+                                </form>
+                                <div className="mt-5 text-center text-red-600">
+                                    {error}
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            {/* {
                 update ? (<div className="min-h-screen py-10">
                     <div className="container mx-auto">
                         <div className=" lg:flex-row w-full lg:w-8/12 bg-white rounded-xl mx-auto shadow-lg overflow-hidden">
@@ -177,7 +223,7 @@ const Settings = () => {
                         </div>
                     </div>
                 </div>)
-            }
+            } */}
 
 
 
